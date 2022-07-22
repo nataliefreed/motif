b336149a40cde960268fd322fd5469c169ce8c0b
Natalie Freed <saplingsonwheels@gmail.com>
2022-07-22 13:27:05 -0500

    Tab shaped category buttons, polygon function

--- src/scripts.js
+++ src/scripts.js
@@ -1,72 +1,9 @@
-// // JOY
-// var joy = Joy({
-// 	// Where the Joy editor goes:
-// 	container: "#joy",
-
-// 	// The words and widgets inside the editor:
-// 	init: "To create my design: "+
-// 		  "{id:'actions', type:'actions'} "+ // a list of actions
-// 		//   "Fill with {id:'color', type:'color', placeholder:[0.3, 0.8, 1.0]}" +
-// 		  "<hr> {type:'save'}", // a save button!
-    
-//     // Load data from URL, otherwise blank:
-// 	data: data,
-
-// 	// Actions to include:
-// 	modules: ["motif", "instructions", "math"],
-
-// 	previewActions: true,
-//     previewNumbers: true,
-
-// 	// What to do when the user makes a change:
-// 	onupdate: function(my){
-// 		// console.log(my.data.color);
-// 		// color = my.data.color;
-// 		// turtle.start();
-// 		p5sketch.clear();
-// 		my.actions.act(p5sketch);
-// 		// my.actions.act(target);
-// 		// motif.draw();
-// 	}
-// });
-
-
-// Joy.add({
-// 	name: "Fill",
-// 	type: "motif/solid fill",
-// 	tags: ["motif", "action"],
-
-// 	init: "Fill with {id:'color', type:'color', placeholder:[50, 0.8, 1.0]}",
-
-// 	// Callback
-// 	onact: function(my){
-// 		my.target.addFill(my.data.color); // "my.target" will be the p5 canvas
-// 	}
-// });
-// Joy.add({
-// 	name: "Add polka dot",
-// 	type: "motif/dot",
-// 	tags: ["motif", "action"],
-
-// 	init: `Add polka dot of radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
-// 	at ({id:'x', type:'number', placeholder:200}, {id:'y', type:'number', placeholder:200})`,
-
-// 	// Callback
-// 	onact: function(my){
-// 		// my.target.background(my.data.color);
-// 		// color = my.data.color;
-// 		my.target.addCircle(my.data.color, my.data.x, my.data.y, my.data.radius);
-// 		// my.target.fill(my.data.color); // "my.target" will be the p5 canvas
-// 	},
-// });
-
-
 window.addEventListener('load', e => {
 	const effectList = [
 		{
 			name: 'solid fill',
 			dropdownName: 'Solid Fill',
-			category: 'Background',
+			category: 'Backgrounds',
 			init: "Fill with {id:'color', type:'color', placeholder:[50, 0.8, 1.0]}",
 			cursor: './assets/cursors/fill-drip-solid.svg',
 			onact: (my) => {
@@ -76,7 +13,7 @@ window.addEventListener('load', e => {
 		{
 			name: 'gradient',
 			dropdownName: 'Gradient',
-			category: 'Background',
+			category: 'Backgrounds',
 			init: "Fill with {id:'color', type:'color', placeholder:[50, 0.8, 1.0]}",
 			cursor: './assets/cursors/fill-drip-solid.svg',
 			onact: (my) => {
@@ -86,8 +23,8 @@ window.addEventListener('load', e => {
 		{
 			name: 'circle',
 			dropdownName: 'Circle',
-			category: 'Shape',
-			init: `Add polka dot of radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
+			category: 'Shapes',
+			init: `Draw polka dot of radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
 			at ({id:'x', type:'number', placeholder:200}, {id:'y', type:'number', placeholder:200})`,
 			cursor: './assets/cursors/star-solid.svg',
 			onact: (my) => {
@@ -97,8 +34,8 @@ window.addEventListener('load', e => {
 		{
 			name: 'square',
 			dropdownName: 'Square',
-			category: 'Shape',
-			init: `Add square of radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
+			category: 'Shapes',
+			init: `Draw square of radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
 			at ({id:'x', type:'number', placeholder:200}, {id:'y', type:'number', placeholder:200})`,
 			cursor: './assets/cursors/star-solid.svg',
 			onact: (my) => {
@@ -108,8 +45,19 @@ window.addEventListener('load', e => {
 		{
 			name: 'polygon',
 			dropdownName: 'Polygon',
-			category: 'Shape',
-			init: `Add polygon of radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
+			category: 'Shapes',
+			init: `Draw polygon with {id:'nsides', type:'number', placeholder:6} sides and radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
+			at ({id:'x', type:'number', placeholder:200}, {id:'y', type:'number', placeholder:200})`,
+			cursor: './assets/cursors/star-solid.svg',
+			onact: (my) => {
+				my.target.polygon(my.data.color, my.data.x, my.data.y, my.data.radius, my.data.nsides);
+			}
+		},
+		{
+			name: 'sparkle',
+			dropdownName: 'Sparkle',
+			category: 'Effects',
+			init: `Add sparkle of radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
 			at ({id:'x', type:'number', placeholder:200}, {id:'y', type:'number', placeholder:200})`,
 			cursor: './assets/cursors/star-solid.svg',
 			onact: (my) => {
@@ -119,8 +67,6 @@ window.addEventListener('load', e => {
 	]
 
 	const app = new MotifApp(effectList);
-
-
 });
 
 class MotifApp {
@@ -258,9 +204,6 @@ class MotifApp {
 
 	initP5() {
 		return new p5(s => {
-			let x = 100;
-			let y = 100;
-		  
 			s.setup = () => {
 			  s.createCanvas(600, 600);
 			  s.background(255);
@@ -269,11 +212,6 @@ class MotifApp {
 			};
 		  
 			s.draw = () => {
-				if(!s.setupFinished) return;
-				// s.background(255);
-			//   s.background(0);
-			//   s.fill(255);
-			//   s.rect(x,y,50,50);
 			};
 		
 			s.clear = () => {
@@ -292,6 +230,19 @@ class MotifApp {
 				s.fill(color);
 				s.noStroke();
 				s.rect(0, 0, s.width, s.height);
+			}
+			s.polygon = (color, x, y, r, nsides) => {
+				if(!s.setupFinished) return;
+				nsides = Math.abs(nsides);
+				s.fill(color);
+				let angle = s.TWO_PI / nsides;
+				s.beginShape();
+				for (let a = 0; a < s.TWO_PI; a += angle) {
+				  let sx = x + s.cos(a) * r;
+				  let sy = y + s.sin(a) * r;
+				  s.vertex(sx, sy);
+				}
+				s.endShape(s.CLOSE);
 			}
 		  }, 'drawing-canvas');
 	}
@@ -314,55 +265,7 @@ class MotifApp {
 					onact: effect.onact
 				});
 			});
-
-			// Joy.add({
-			// 	name: "Fill",
-			// 	type: "motif/solid fill",
-			// 	tags: ["motif", "action"],
-			
-			// 	init: "Fill with {id:'color', type:'color', placeholder:[50, 0.8, 1.0]}",
-			
-			// 	// Callback
-			// 	onact: function(my){
-			// 		my.target.addFill(my.data.color); // "my.target" will be the p5 canvas
-			// 	}
-			// });
-			// Joy.add({
-			// 	name: "Add polka dot",
-			// 	type: "motif/dot",
-			// 	tags: ["motif", "action"],
-			
-			// 	init: `Add polka dot of radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
-			// 	at ({id:'x', type:'number', placeholder:200}, {id:'y', type:'number', placeholder:200})`,
-			
-			// 	// Callback
-			// 	onact: function(my){
-			// 		// my.target.background(my.data.color);
-			// 		// color = my.data.color;
-			// 		my.target.addCircle(my.data.color, my.data.x, my.data.y, my.data.radius);
-			// 		// my.target.fill(my.data.color); // "my.target" will be the p5 canvas
-			// 	},
-			// });
-		
-			// Joy.add({
-			// 	name: "Brush",
-			// 	type: "motif/brush",
-			// 	tags: ["motif", "action"],
-			
-			// 	init: "Brush {id:'choose', type:'choose', options:['a','b','c'], placeholder:'a'} in color {id:'color', type:'color', placeholder:[150, 0.8, 1.0]}",
-			
-			// 	// Callback
-			// 	onact: function(my){
-			// 		// my.target.background(my.data.color);
-			// 		color = my.data.color;
-			// 		my.target.addCircle(my.data.color, my.data.x, my.data.y, my.data.radius);
-			// 		// my.target.fill(my.data.color); // "my.target" will be the p5 canvas
-			// 		//TODO: this is going to need a list of points
-			// 	},
-			// });
 		});
-
-		
 
 		let joy = Joy({
 			// Where the Joy editor goes:
@@ -444,220 +347,3 @@ let _rgbStrToHsl = rgbStr => {
 		b: parseInt(result[3], 16)
 	} : null;
 	}
-
-// class Effect {
-// 	constructor(name, applyEffect, settings, category) {
-// 		this.name = name;
-// 		this.applyEffect = applyEffect;
-// 		this.category = category;
-// 		this.settings = settings;
-// 	  }
-	
-// 	  getButtonHTML() {
-// 		let b = document.createElement("input");
-// 		b.setAttribute("type", "button");
-// 		b.classList.add('effect-button');
-// 		b.setAttribute("value", this.name);
-// 		b.setAttribute("id", this.name);
-// 		return b;
-// 	  }
-	
-// 	  getName() {
-// 		return this.name;
-// 	  }
-	
-// 	  cloneSettings() {
-// 		let copy = { };
-// 		for(const name in this.settings) {
-// 		  copy[name] = this.settings[name].clone();
-// 		}
-// 		return copy;
-// 	  }
-	
-// 	  clone() {
-// 		return new Effect(this.name, this.applyEffect, this.cloneSettings(), this.category);
-// 	  }
-	
-// 	  createEvent(startPosition, time) {
-// 		return new DrawingEvent(this.clone(), startPosition, time);
-// 	  }
-// }
-
-
-
-// // TURTLE
-// window.turtle = new Turtle({
-// 	width:400, height:400,
-// 	data: data
-// });
-// // document.querySelector("#turtle").appendChild(turtle.canvas);
-
-// // JOY ACTIONS
-// Joy.add({
-// 	name: "Move",
-// 	type: "turtle/forward",
-// 	tags: ["turtle", "action"],
-
-// 	// What the action does is EMBEDDED IN A PLAIN-LANGUAGE SENTENCE
-// 	init: "Move forward {id:'steps', type:'number', placeholder:50} steps",
-
-// 	// Callback
-// 	onact: function(my){
-// 		my.target.forward(my.data.steps); // "my.target" will be the turtle
-// 	}
-// });
-
-// Joy.add({
-// 	name: "Turn",
-// 	type: "turtle/turn",
-// 	tags: ["turtle", "action"],
-// 	init: "Turn {id:'angle', type:'number', placeholder:10} degrees",
-// 	onact: function(my){
-// 		my.target.turn(my.data.angle);
-// 	}
-// });
-
-// Joy.add({
-// 	name: "Change color",
-// 	type: "turtle/color",
-// 	tags: ["turtle", "action"],
-// 	init: "Change color to {id:'color', type:'color'}",
-// 	onact: function(my){
-// 		my.target.setColor(my.data.color);
-// 	}
-// });
-
-// // JOY
-// window.joy = Joy({
-// 	// Where the Joy editor goes:
-// 	container: "#joy",
-
-// 	// The words &amp; widgets inside the editor:
-// 	init: "To create my design: "+
-// 		  "{id:'turtleInstructions', type:'actions'} "+ // a list of actions
-// 		  "<hr> {type:'save'}", // a save button!
-    
-//     // Load data from URL, otherwise blank:
-// 	data: data,
-
-// 	// Other actions to include, beyond turtle actions:
-// 	modules: ["instructions", "math"],
-
-// 	// What to do when the user makes a change:
-// 	onupdate: function(my){
-// 		turtle.start();
-// 		my.turtleInstructions.act(turtle);
-// 		turtle.draw();
-// 	}
-// });
-
-// P.S: Dragging the turtle around...
-// turtle.ondrag = function(){
-// 	joy.update();
-// };
-
-// window.addEventListener('load', (event) =>{
-//     var joy = Joy({
-//         // Where the Joy editor goes:
-//         container: "#joy",
-
-//         // The words & actors inside the editor:
-//         init: "Make the box "+
-//             // Define actors with JSON/object-literal notation:
-//             "{id:'width', type:'number', placeholder:50}px wide, "+
-//             "{id:'height', type:'number', placeholder:50}px tall, "+
-//             "and colored {id:'color', type:'color'}",
-
-//         // What to do when the user makes a change:
-//         onupdate: function(my){
-//             var box = document.getElementById("box");
-//             box.style.width = my.data.width;
-//             box.style.height = my.data.height;
-//             box.style.background = my.data.color;
-//             console.log(my.data.width, my.data.height);
-//         }
-//     });
-// });
-
-// window.joy = Joy({
-// 	init: "Make the box {id:'width', type:'number', placeholder:50}px wide, "+
-// 		  "{id:'height', type:'number', placeholder:50}px tall, "+
-// 		  "and colored {id:'color', type:'color'}",
-// 	container: "#joy",
-// 	onupdate: function(my){
-// 		var box = document.getElementById("box");
-//         box.style.width = my.data.width + "px";
-//         box.style.height = my.data.height + "px";
-//         box.style.background = my.data.color;
-// 	}
-// });
-
-// let effects = {
-// 	'fill-button': 0,
-// 	'dot-button': 1
-// }
-
-// function applyEffect(effectNum, settings) {
-// 	// let items = document.getElementsByClassName('joy-bullet');
-// 	// let lastItem = items[items.length-1];
-// 	// lastItem.click();
-// 	// let chooser = document.getElementsByClassName('joy-modal-chooser')[0];
-// 	// chooser.firstChild.firstChild.children[effectNum].click();
-// 	switch(effectNum) {
-// 		case 0:
-// 			joy.actions.addAction('motif/fill',undefined, {color:{ type:'color', value:[50, 0.5, 1.0]}});
-// 			joy.actions.update();
-// 			break;
-// 		case 1:
-// 			let params = {
-// 				color: { type:'color', value:[50, 0.5, 1.0]},
-// 				x: { type:'number', value: Math.round(settings.x)},
-// 				y: {type:'number', value: Math.round(settings.y)}
-// 			}
-// 			// params = JSON.stringify(params);
-// 			joy.actions.addAction('motif/dot',undefined, params);
-// 			joy.actions.update();
-// 			break;
-// 		default: break;
-// 	}
-// 	//TODO: this is where to add to joy data / list of commands
-// }
-
-
-// let p5sketch = new p5(s => {
-// 	let x = 100;
-// 	let y = 100;
-  
-// 	s.setup = () => {
-// 	  s.createCanvas(600, 600);
-// 	  s.background(255);
-// 	  s.setupFinished = true;
-// 	  s.noLoop();
-// 	};
-  
-// 	s.draw = () => {
-// 		if(!s.setupFinished) return;
-// 	    // s.background(255);
-// 	//   s.background(0);
-// 	//   s.fill(255);
-// 	//   s.rect(x,y,50,50);
-// 	};
-
-// 	s.clear = () => {
-// 		if(!s.setupFinished) return;
-// 	    s.background(255);
-// 	}
-
-// 	s.addCircle = (color, x, y, r) => {
-// 		if(!s.setupFinished) return;
-// 	    s.fill(color);
-// 		s.circle(x, y, r*2);
-// 	}
-
-// 	s.addFill = (color) => {
-// 		if(!s.setupFinished) return;
-// 	    s.fill(color);
-// 		s.noStroke();
-// 		s.rect(0, 0, s.width, s.height);
-// 	}
-//   }, 'drawing-canvas');
