f261c064ce2df859345be1a9d0f0087c884583e9
Natalie Freed <saplingsonwheels@gmail.com>
2022-07-25 12:38:02 -0500

    Add drawing tools - star, heart, square, filter

--- src/scripts.js
+++ src/scripts.js
@@ -14,17 +14,39 @@ window.addEventListener('load', e => {
 			name: 'gradient',
 			dropdownName: 'Gradient',
 			category: 'Backgrounds',
-			init: "Fill with {id:'color', type:'color', placeholder:[50, 0.8, 1.0]}",
+			init: "Gradient from {id:'color1', type:'color', placeholder:[50, 0.8, 1.0]} to {id:'color2', type:'color', placeholder:[100, 0.8, 1.0]}",
 			cursor: './assets/cursors/fill-drip-solid.svg',
 			onact: (my) => {
-				my.target.addFill(my.data.color);
+				my.target.gradient(my.data.color1, my.data.color2);
+			}
+		},
+		{
+			name: 'paper doll',
+			dropdownName: 'Paper doll',
+			category: 'Stencils',
+			init: `Paper doll with radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
+			at ({id:'x', type:'number', placeholder:200}, {id:'y', type:'number', placeholder:200})`,
+			cursor: './assets/cursors/star-solid.svg',
+			onact: (my) => {
+				my.target.addCircle(my.data.color, my.data.x, my.data.y, my.data.radius);
+			}
+		},
+		{
+			name: 'box',
+			dropdownName: 'Box',
+			category: 'Stencils',
+			init: `Box with radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
+			at ({id:'x', type:'number', placeholder:200}, {id:'y', type:'number', placeholder:200})`,
+			cursor: './assets/cursors/star-solid.svg',
+			onact: (my) => {
+				my.target.addCircle(my.data.color, my.data.x, my.data.y, my.data.radius);
 			}
 		},
 		{
 			name: 'circle',
 			dropdownName: 'Circle',
 			category: 'Shapes',
-			init: `Draw polka dot of radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
+			init: `Circle of radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
 			at ({id:'x', type:'number', placeholder:200}, {id:'y', type:'number', placeholder:200})`,
 			cursor: './assets/cursors/star-solid.svg',
 			onact: (my) => {
@@ -35,18 +57,18 @@ window.addEventListener('load', e => {
 			name: 'square',
 			dropdownName: 'Square',
 			category: 'Shapes',
-			init: `Draw square of radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
+			init: `Square of size {id:'size', type:'number', placeholder:40} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
 			at ({id:'x', type:'number', placeholder:200}, {id:'y', type:'number', placeholder:200})`,
 			cursor: './assets/cursors/star-solid.svg',
 			onact: (my) => {
-				my.target.addCircle(my.data.color, my.data.x, my.data.y, my.data.radius);
+				my.target.addSquare(my.data.color, my.data.x, my.data.y, my.data.size);
 			}
 		},
 		{
 			name: 'polygon',
 			dropdownName: 'Polygon',
 			category: 'Shapes',
-			init: `Draw polygon with {id:'nsides', type:'number', placeholder:6} sides and radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
+			init: `Polygon with {id:'nsides', type:'number', placeholder:6} sides and radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
 			at ({id:'x', type:'number', placeholder:200}, {id:'y', type:'number', placeholder:200})`,
 			cursor: './assets/cursors/star-solid.svg',
 			onact: (my) => {
@@ -54,11 +76,75 @@ window.addEventListener('load', e => {
 			}
 		},
 		{
-			name: 'sparkle',
-			dropdownName: 'Sparkle',
-			category: 'Effects',
-			init: `Add sparkle of radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
+			name: 'star',
+			dropdownName: 'Star',
+			category: 'Shapes',
+			init: `Star with {id:'npoints', type:'number', placeholder:7} points, outer {id:'r1', type:'number', placeholder:20}, inner {id:'r2', type:'number', placeholder:10} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
 			at ({id:'x', type:'number', placeholder:200}, {id:'y', type:'number', placeholder:200})`,
+			cursor: './assets/cursors/star-solid.svg',
+			onact: (my) => {
+				my.target.star(my.data.color, my.data.x, my.data.y, my.data.r1, my.data.r2, my.data.npoints);
+			}
+		},
+		{
+			name: 'heart',
+			dropdownName: 'Heart',
+			category: 'Shapes',
+			init: `Heart of size {id:'size', type:'number', placeholder:40} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
+			at ({id:'x', type:'number', placeholder:200}, {id:'y', type:'number', placeholder:200})`,
+			cursor: './assets/cursors/star-solid.svg',
+			onact: (my) => {
+				my.target.heart(my.data.color, my.data.x, my.data.y, my.data.size);
+			}
+		},
+		{
+			name: 'marker',
+			dropdownName: 'Marker',
+			category: 'Brushes',
+			init: `Marker with radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
+			at ({id:'x', type:'number', placeholder:200}, {id:'y', type:'number', placeholder:200})`,
+			cursor: './assets/cursors/star-solid.svg',
+			onact: (my) => {
+				my.target.addCircle(my.data.color, my.data.x, my.data.y, my.data.radius);
+			}
+		},
+		{
+			name: 'rainbow brush',
+			dropdownName: 'Rainbow Brush',
+			category: 'Brushes',
+			init: `Rainbow brush with radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
+			at ({id:'x', type:'number', placeholder:200}, {id:'y', type:'number', placeholder:200})`,
+			cursor: './assets/cursors/star-solid.svg',
+			onact: (my) => {
+				my.target.addCircle(my.data.color, my.data.x, my.data.y, my.data.radius);
+			}
+		},
+		{
+			name: 'swirl',
+			dropdownName: 'Swirl',
+			category: 'Effects',
+			init: `Add swirl of radius {id:'radius', type:'number', placeholder:20} in color {id:'color', type:'color', placeholder:[20, 0.8, 1.0]}  
+			at ({id:'x', type:'number', placeholder:200}, {id:'y', type:'number', placeholder:200})`,
+			cursor: './assets/cursors/star-solid.svg',
+			onact: (my) => {
+				my.target.addCircle(my.data.color, my.data.x, my.data.y, my.data.radius);
+			}
+		},
+		{
+			name: 'filter',
+			dropdownName: 'Filter',
+			category: 'Effects',
+			init: `Color shift of type {id:'filter', type:'choose', options:['gray','threshold','opaque', 'invert', 'posterize', 'dilate', 'blur', 'erode'], placeholder:'invert'}`,
+			cursor: './assets/cursors/star-solid.svg',
+			onact: (my) => {
+				my.target.applyFilter(my.data.filter);
+			}
+		},
+		{
+			name: 'tidy',
+			dropdownName: 'Tidy',
+			category: 'Effects',
+			init: `Tidy`,
 			cursor: './assets/cursors/star-solid.svg',
 			onact: (my) => {
 				my.target.addCircle(my.data.color, my.data.x, my.data.y, my.data.radius);
@@ -77,6 +163,111 @@ class MotifApp {
 		this.sketch.clear('#aaaaaa');
 		this.joy = this.initJoy();
 		this.initUI();
+	}
+
+	initP5() {
+		return new p5(s => {
+			s.setup = () => {
+			  s.createCanvas(600, 600);
+			  s.background(255);
+			  s.setupFinished = true;
+			  s.noLoop();
+			};
+		  
+			s.draw = () => {
+			};
+		
+			s.clear = () => {
+				if(!s.setupFinished) return;
+				s.background(255);
+			}
+		
+			s.addCircle = (color, x, y, r) => {
+				if(!s.setupFinished) return;
+				s.fill(color);
+				s.noStroke();
+				s.circle(x, y, r*2);
+			}
+
+			s.addSquare = (color, x, y, size) => {
+				s.push();
+				s.rectMode(s.CENTER);
+				s.fill(color);
+				s.noStroke();
+				s.rect(x, y, size, size);
+				s.pop();
+			}
+		
+			s.addFill = (color) => {
+				if(!s.setupFinished) return;
+				s.fill(color);
+				s.noStroke();
+				s.rect(0, 0, s.width, s.height);
+			}
+			s.star = (color, x, y, r1, r2, npoints) => {
+				if(!s.setupFinished) return;
+				let angle = s.TWO_PI / npoints;
+				let halfAngle = angle / 2.0;
+				s.fill(color);
+				s.noStroke();
+				s.beginShape();
+				for (let a = 0; a < s.TWO_PI; a += angle) {
+				  let sx = x + s.cos(a) * r2;
+				  let sy = y + s.sin(a) * r2;
+				  s.vertex(sx, sy);
+				  sx = x + s.cos(a + halfAngle) * r1;
+				  sy = y + s.sin(a + halfAngle) * r1;
+				  s.vertex(sx, sy);
+				}
+				s.endShape(s.CLOSE);
+			}
+
+			s.polygon = (color, x, y, r, nsides) => {
+				if(!s.setupFinished) return;
+				nsides = Math.abs(nsides);
+				s.fill(color);
+				s.noStroke();
+				let angle = s.TWO_PI / nsides;
+				s.beginShape();
+				for (let a = 0; a < s.TWO_PI; a += angle) {
+				  let sx = x + s.cos(a) * r;
+				  let sy = y + s.sin(a) * r;
+				  s.vertex(sx, sy);
+				}
+				s.endShape(s.CLOSE);
+			}
+
+			s.gradient = (color1, color2) => {
+				if(!s.setupFinished) return;
+				let pcolor1 = s.color(color1);
+				let pcolor2 = s.color(color2);
+				s.noFill();
+				s.strokeWeight(1);
+				for(let i=0;i<s.height;i++) {
+					s.stroke(s.lerpColor(pcolor1, pcolor2, i/s.height));
+					s.line(0, i, s.width, i);
+				}
+			}
+
+			s.heart = (color, x, y, size) => {
+				if(!s.setupFinished) return;
+				s.push();
+				s.translate(0, -size/2);
+				s.fill(color);
+				s.noStroke();
+				s.beginShape();
+				s.vertex(x, y);
+				s.bezierVertex(x - size / 2, y - size / 2, x - size, y + size / 3, x, y + size);
+				s.bezierVertex(x + size, y + size / 3, x + size / 2, y - size / 2, x, y);
+				s.endShape(s.CLOSE);
+				s.pop();
+			}
+
+			s.applyFilter = (filter) => {
+				s.filter(s[filter.toUpperCase()]);
+			}
+
+		  }, 'drawing-canvas');
 	}
 
 	initUI() {
@@ -119,17 +310,12 @@ class MotifApp {
 		//Mouse event listeners
 
 		let dragging = false;
+		let mouseDownOverCanvas = false; //TODO: doesn't work if you leave canvas with mouse down
 
 		document.getElementById('drawing-canvas').addEventListener('mousedown', e => {
 			dragging = false;
-		});
+			mouseDownOverCanvas = true;
 
-		document.getElementById('drawing-canvas').addEventListener('mousemove', e => {
-			dragging = true;
-		});
-
-		document.getElementById('drawing-canvas').addEventListener('mouseup', e => {
-			// console.log(drag ? 'drag' : 'click');
 			let activeEffect = this.getSelectedEffect();
 			console.log(activeEffect);
 			if(activeEffect) {
@@ -138,20 +324,34 @@ class MotifApp {
 				y: this.sketch.mouseY
 				});
 			}
+
 		});
+
+		document.getElementById('drawing-canvas').addEventListener('mousemove', e => {
+			if(mouseDownOverCanvas) {
+				dragging = true;
+				// console.log("draggin");
+			}
+		});
+
+		document.getElementById('drawing-canvas').addEventListener('mouseup', e => {
+			// console.log(drag ? 'drag' : 'click');
+			mouseDownOverCanvas = false;
+		});
+
 	}
 
 	addEvent(effectName, settings) {
 		let effect = this.effects[effectName];
 
-		let hexColor = document.getElementById('color-picker').value;
-		let hslColor = _rgbToHsl(_hexToRgb(hexColor));
-		console.log(hslColor);
+		// let hexColor = document.getElementById('color-picker').value;
+		// let hslColor = _rgbToHsl(_hexToRgb(hexColor));
 
 		//TODO: make this match effect params
 		let params = {
 			// color: { type:'color', value:[50, 0.5, 1.0]},
-			color: { type:'color', value:[hslColor[0], hslColor[1], hslColor[2]]},
+			// color: { type:'color', value:[hslColor[0], hslColor[1], hslColor[2]]},
+			color: { type:'color', value:[Math.random()*360, 0.8, 0.8]},
 			x: { type:'number', value: Math.round(settings.x)},
 			y: {type:'number', value: Math.round(settings.y)}
 		}
@@ -200,51 +400,6 @@ class MotifApp {
 			dict[item[key]] = item;
 		});
 		return dict;
-	}
-
-	initP5() {
-		return new p5(s => {
-			s.setup = () => {
-			  s.createCanvas(600, 600);
-			  s.background(255);
-			  s.setupFinished = true;
-			  s.noLoop();
-			};
-		  
-			s.draw = () => {
-			};
-		
-			s.clear = () => {
-				if(!s.setupFinished) return;
-				s.background(255);
-			}
-		
-			s.addCircle = (color, x, y, r) => {
-				if(!s.setupFinished) return;
-				s.fill(color);
-				s.circle(x, y, r*2);
-			}
-		
-			s.addFill = (color) => {
-				if(!s.setupFinished) return;
-				s.fill(color);
-				s.noStroke();
-				s.rect(0, 0, s.width, s.height);
-			}
-			s.polygon = (color, x, y, r, nsides) => {
-				if(!s.setupFinished) return;
-				nsides = Math.abs(nsides);
-				s.fill(color);
-				let angle = s.TWO_PI / nsides;
-				s.beginShape();
-				for (let a = 0; a < s.TWO_PI; a += angle) {
-				  let sx = x + s.cos(a) * r;
-				  let sy = y + s.sin(a) * r;
-				  s.vertex(sx, sy);
-				}
-				s.endShape(s.CLOSE);
-			}
-		  }, 'drawing-canvas');
 	}
 
 	initJoy() {
