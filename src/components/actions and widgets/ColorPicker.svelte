db46ae4b49d8a8ffe4127718c9a2e0d1f54d9399
Natalie Freed <saplingsonwheels@gmail.com>
2024-04-04 13:34:29 -0500

    New RGB color picker

---
+++ src/components/actions and widgets/ColorPicker.svelte
@@ -0,0 +1,195 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import tinycolor from 'tinycolor2';
+
+  const dispatch = createEventDispatcher();
+
+  export let value: string; //initial value
+
+  let c = tinycolor(value).toRgb();
+  let red = c.r;
+  let green = c.g;
+  let blue = c.b;
+  let alpha = c.a;
+
+  $: color = tinycolor({ r: red, g: green, b: blue, a: alpha });
+
+  $: rgbaString = color.toRgbString();
+  $: rgbString = tinycolor(rgbaString).setAlpha(1).toRgbString();
+  $: gradientRed = `linear-gradient(to right, rgba(0, ${green}, ${blue}, 1), rgba(255, ${green}, ${blue}, 1))`;
+  $: gradientGreen = `linear-gradient(to right, rgba(${red}, 0, ${blue}, 1), rgba(${red}, 255, ${blue}, 1))`;
+  $: gradientBlue = `linear-gradient(to right, rgba(${red}, ${green}, 0, 1), rgba(${red}, ${green}, 255, 1))`;
+  $: gradientAlpha = `linear-gradient(to right, rgba(${red}, ${green}, ${blue}, 0), rgba(${red}, ${green}, ${blue}, 1))`;
+
+  function updateColor() {
+    dispatch('valueChange', { value: color.toRgbString() });
+  }
+</script>
+
+<div class="color-picker">
+
+  <div class="sliders">
+    <div class="slider">
+      <label for="red" style="color:red">Red</label>
+      <input type="number" min="0" max="255" bind:value={red} on:input={updateColor} style="color:red">
+      <input type="range" id="red" min="0" max="255" bind:value={red} on:input={updateColor} style="--slider-gradient: {gradientRed};">
+    </div>
+    <div class="slider">
+      <label for="green" style="color:green">Green</label>
+      <input type="number" min="0" max="255" bind:value={green} on:input={updateColor} style="color:green">
+      <input type="range" id="green" min="0" max="255" bind:value={green} on:input={updateColor} style="--slider-gradient: {gradientGreen};">
+      
+    </div>
+    <div class="slider">
+      <label for="blue" style="color:blue">Blue</label>
+      <input type="number" min="0" max="255" bind:value={blue} on:input={updateColor} style="color:blue">
+      <input type="range" id="blue" min="0" max="255" bind:value={blue} on:input={updateColor} style="--slider-gradient: {gradientBlue};">
+    </div>
+  </div>
+
+  <div class="preview">
+    <div class="color-preview" style="background-color: {rgbaString}; border-color:{rgbString};"></div>
+
+    <div class="slider preview-slider">
+      <input type="range" id="opacity" min="0" max="1" step="0.01" bind:value={alpha} on:input={updateColor} style="--slider-gradient: {gradientAlpha};">
+    </div>
+  </div>
+
+</div>
+
+<style>
+  .color-picker {
+    display: flex;
+    flex-direction: column;
+    margin-top: 10px;
+  }
+  .sliders {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+  }
+
+  .slider {
+    margin-bottom: 17px;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .slider input[type='range'] {
+    appearance: none;
+    background: transparent;
+    height: 20px;
+    border-radius: 6px;
+    background: var(--slider-gradient);
+    outline: none;
+    cursor: pointer;
+  }
+
+  .preview {
+    display: flex;
+    flex-direction: row;
+    margin-top: 10px;
+    margin-bottom: 0px;
+  }
+
+  .preview-slider {
+    flex-grow: 1;
+  }
+
+  .preview-slider input[type='range'] {
+    height: 40px;
+    width: 100%;
+    background: transparent;
+    background: var(--slider-gradient);
+    outline: none;
+    margin: 0;
+  }
+
+  .color-preview {
+    width: 40px;
+    height: 40px;
+    border-radius: 10px;
+    margin-right: 16px;
+    border: 1px solid;
+    box-sizing: border-box;
+  }
+
+  input[type='number'] {
+      margin: 0 5px;
+      font-family: 'FuturaHandwritten';
+      /* font-size: 14px; */
+      border: 1px solid lightgray;
+      border-radius: 4px;          /* rounds the corners slightly */
+      outline: none;               /* removes the blue outline when focused */
+      transition: border 0.3s;    /* smooth transition for border changes */
+      padding: 0 5px;            /* adds some internal spacing */
+  }
+
+  /* Styles for Chrome, Safari, and Opera */
+  .slider input[type='range']::-webkit-slider-runnable-track {
+    appearance: none;
+    -webkit-appearance: none;
+    background: var(--slider-gradient);
+  }
+
+  .preview-slider input[type='range']::-webkit-slider-runnable-track {
+    appearance: none;
+    width: 0;
+    height: 0;
+    -webkit-appearance: none;
+    background: var(--slider-gradient);
+  }
+
+  /* Styles for Firefox */
+  .slider input[type='range']::-moz-range-track {
+    appearance: none;
+    background: var(--slider-gradient);
+  }
+
+  /* Styles for IE */
+  .slider input[type='range']::-ms-track {
+    width: 0;
+    height: 0;
+    background: var(--slider-gradient);
+  }
+
+  /* Thumb styles */
+  .slider input[type='range']::-webkit-slider-thumb {
+    appearance: none;
+    background: #ffffff;
+    border: 1px solid #ccc;
+    border-radius: 50%;
+  }
+
+    /* Thumb styles */
+  .preview-slider input[type='range']::-webkit-slider-thumb {
+    appearance: none;
+    width: 12px;
+    height: 34px;
+    background: #ffffff;
+    border: 1px solid #ccc;
+    border-radius: 6px;
+    transform: translateY(-12px);
+  }
+
+  /***** Focus Styles *****/
+  /* Removes default focus */
+  input[type="range"]:focus {
+    outline: none;
+  }
+
+  /***** Chrome, Safari, Opera, and Edge Chromium *****/
+  input[type="range"]:focus::-webkit-slider-thumb {
+    border: 1px solid #ccc;
+    outline: 3px solid rgb(255, 255, 255);
+    outline-offset: 0.125rem;
+  }
+
+  /******** Firefox ********/
+  input[type="range"]:focus::-moz-range-thumb {
+    border: 1px solid #ccc;
+    outline: 3px solid #ffffff;
+    outline-offset: 0.125rem;     
+  }
+
+</style>
--- src/components/actions and widgets/ColorWidget.svelte
+++ src/components/actions and widgets/ColorWidget.svelte
@@ -3,6 +3,8 @@
   import tinycolor from 'tinycolor2';
   import { selectedCodeEffect } from '../../stores/dataStore';
   import { createEventDispatcher } from 'svelte';
+  import ColorPicker from './ColorPicker.svelte';
+  import Tooltip from '../Tooltip.svelte';
 
   export let id = '';
   export let value = '#FFFFFF';
@@ -10,28 +12,7 @@
 
   const dispatch = createEventDispatcher();
 
-  let colorOptions = [
-    '#FF0000ff', // Red
-    '#FFA500ff', // Orange
-    '#FFD700ff', // Gold
-    '#808000ff', // Olive
-    '#008000ff', // Green
-    '#26A2E0ff', // Light blue
-    '#0000FFff', // Blue
-    '#4B0082ff', // Indigo
-    '#800080ff', // Purple
-    '#EE82EEff', // Violet
-    '#FFC0CBff', // Pink
-    '#808080ff', // Gray
-    '#FFFFFFff', // White
-    '#000000ff', // Black
-    '#5E2109ff', // Brown
-    '#D2B48Cff'  // Tan
-  ];
-
   let colorButton: HTMLElement;
-  let hiddenColorInput: HTMLInputElement;
-  let firstUpdate = false; // to save initial state to undo queue
 
   onMount(() => {
     colorButton.style.background = tinycolor(value).toHexString();
@@ -45,15 +26,8 @@
     const color = tinycolor(hexColor);
     const hue = color.toHsv().h;
     filterStyle = `hue-rotate(${hue}deg)`;
-    dispatch('valueChange', { id, value: hexColor, save: firstUpdate });
-    firstUpdate = false;
+    dispatch('valueChange', { id, value: hexColor });
   }
-
-  // function updateColorButton(hexColor: string) {
-  //   colorButton.style.background = tinycolor(hexColor).toHexString();
-  //   dispatch('valueChange', { id, value: hexColor, save: firstUpdate });
-  //   firstUpdate = false;
-  // }
 
   function randomize() {
     var randomColor = tinycolor.random();
@@ -64,74 +38,47 @@
 
   function handleClick(event: Event) {
     updateColorButton(savedValue);
+    clearInterval(oscillateID);
     previewEnd = true;
-    
-    if($selectedCodeEffect === "random") {
-      randomize();
-    }
-    else if($selectedCodeEffect === "point") {
-      openColorPicker();
-    }
   }
 
-  function openColorPicker() {
-    firstUpdate = true;
-    hiddenColorInput.click();
-    firstUpdate = false;
-  }
-
-  function handleColorChange(event: Event) {
-    const input = event.target as HTMLInputElement;
-    value = input.value;
+  function handleColorChange(event: CustomEvent) {
+    value = event.detail.value;
     updateColorButton(value);
   }
 
-  // let oscillateID: number;
+  let oscillateID;
   function handleMouseOver(event: Event) {
     savedValue = value;
-    let newValue = tinycolor(value);
-    newValue.spin(10);
-    if(newValue.isDark()) newValue.brighten(20);
-    else newValue.darken(20);
+    // let newValue = tinycolor(value);
+    // newValue.spin(10);
+    // if(newValue.isDark()) newValue.brighten(20);
+    // else newValue.darken(20);
     previewEnd = false;
-    updateColorButton(newValue.toHexString());
+    // updateColorButton(newValue.toRgbString());
     // oscillate value
-    // oscillateID = window.setInterval(() => {
-    //   updateColorButton(tinycolor(value).spin(1).toHexString());
-    // }, 20);
+    oscillateID = setInterval(() => {
+      updateColorButton(tinycolor(value).spin(1).toHexString());
+    }, 20);
   }
   function handleMouseOut(event: Event) {
     //console.log("returning to saved value!");
-    // clearInterval(oscillateID);
+    clearInterval(oscillateID);
     if(!previewEnd) {
       updateColorButton(savedValue);
       previewEnd = true;
     }
   }
 
-  $: cursorStyle = ($selectedCodeEffect === null || $selectedCodeEffect === 'point') ? 'pointer' : '';
-
 </script>
 
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<!-- <span
-  class="color-palette-widget"
-  bind:this={colorButton}
-  on:click={handleClick}
-  on:mouseover={handleMouseOver}
-  on:mouseout={handleMouseOut}
-  style="background-color: {value}; cursor: {cursorStyle};"
-  id={id}
-> -->
-
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-<span>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
 <span
   bind:this={colorButton}
   on:click={handleClick}
   on:mouseover={handleMouseOver}
   on:mouseout={handleMouseOut}
-  style="cursor: {cursorStyle};"
   id={id}
   class="color-palette-widget"
 >
@@ -139,34 +86,18 @@
   <div class="color-overlay" style="background-color: {value};"></div>
 </span>
 
-<input
-  type="color"
-  bind:this={hiddenColorInput}
-  class="hidden-color-picker"
-  on:input={handleColorChange}
-  value={value}
-/>
-</span>
-
-<!-- </span> -->
-
-
+{#if colorButton}
+  <Tooltip element={colorButton}>
+    <div class="color-picker">
+      <ColorPicker {value} on:valueChange={handleColorChange} />
+   </div>
+  </Tooltip>
+{/if}
 
 
 <style>
 
-  .hidden-color-picker {
-    width: 1px;
-    height: 1px;
-    margin-left: 1em;
-    margin-right: 0em;
-    border: none;
-    color: transparent;
-    background-color: transparent;
-    fill: transparent;
-  }
-
-  .color-palette-widget {
+.color-palette-widget {
     position: relative;
     width: 1.7em;
     /* height: 1.5em; */
@@ -176,6 +107,10 @@
     vertical-align: middle;
     transform: translateY(-0.1em);
     -webkit-transform: translateY(-0.1em);
+}
+
+.color-picker {
+  padding: 10px;
 }
 
 .splotch-image {
@@ -193,20 +128,3 @@
   mix-blend-mode: overlay;
 }
 </style>
-
-
-  <!-- <script lang="ts">
-  import { createEventDispatcher } from 'svelte';
-
-  export let id = '';
-  export let value = '#FFFFFF';
-
-  const dispatch = createEventDispatcher();
-
-  function handleChange(event: Event) {
-    const target = event.target as HTMLInputElement;
-    dispatch('valueChange', { id, value: target.value });
-  }
-</script>
-
-<input type="color" bind:value={value} on:input={handleChange} id={id} /> -->
