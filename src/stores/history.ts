3f50ed850f0d68ae0acfb96030bb271fc7adfd45
Natalie Freed <saplingsonwheels@gmail.com>
2024-05-10 23:11:28 -0500

    Undo redo fixes

--- src/stores/history.ts
+++ src/stores/history.ts
@@ -6,6 +6,7 @@ import { deepCopy } from '../utils/utils';
 import deepEqual from 'deep-equal';
 
 type Storable = {
+  note: string;
   actionStore: ActionStore;
   stagedActionID: string; // uuid
 };
@@ -13,30 +14,53 @@ type Storable = {
 // Initialize the history store
 export const historyStore = createHistoryStore();
 let current:Storable;
-let initialState:Storable;
+// let initialState:Storable;
+let paused = false;
 
 // Call after stores are populated with initial data
-function initHistoryStore() {
-  initialState = copyCurrentState();
-  current = copyCurrentState();
-}
+// function initHistoryStore() {
+//   initialState = copyCurrentState();
+//   current = copyCurrentState();
+// }
 
 function createHistoryStore() {
   const { subscribe, update, set } = writable<{ past: Storable[], future: Storable[] }>({ past: [], future: [] });
 
   return {
     subscribe,
+    pause: () => {
+      paused = true;
+    },
+    resume: () => {
+      paused = false;
+    },
     // add a new state to the history
     push: (newState: Storable) => {
-      if(!initialState) {
-        initHistoryStore(); //initialize when first item pushed to history
-        // console.log("initial state", initialState);
+      if(paused) {
+        // console.log("history paused");
+        return;
       }
+      // console.log(`pushing to history: ${newState.note}`);
+      // if(!initialState) {
+      //   initHistoryStore(); //initialize when first item pushed to history
+      //   // console.log("initial state", initialState);
+      // }
       update(({ past, future }) => {
-        if (!deepEqual(current, newState)) {
+        // console.log("action store length", Object.keys(current.actionStore).length);
+        //if states are not the same, add to history
+        // let stagedActionChanged = !deepEqual(current.actionStore[current.stagedActionID], newState.actionStore[newState.stagedActionID]);
+        if (!current) {
           current = deepCopy(newState);
-          return { past: [...past, current], future: [] };
+          return { past: [...past], future: [] };
+        } else if (differentOtherThanStagedAction(current, newState)) {
+          // console.log("different enough to save to history");
+          // console.log("saved state", newState);
+          const prevState = current;
+          current = deepCopy(newState);
+          // console.log("past notes", past.map(state => state.note));
+          return { past: [...past, prevState], future: [] };
         }
+        // console.log("not different enough to save to history");
         return { past, future };
       });
     },
@@ -44,24 +68,35 @@ function createHistoryStore() {
     // undo and return the new state to be applied
     // save current state to redo stack
     pop: (): Storable | null => {
-      console.log("popping from history");
       let retrievedState: Storable | null = null;
       update(({ past, future }) => {
         if (past.length === 0) {
           // nothing to undo
+          console.log("nothing to undo");
           return { past, future };
         }
-        else if (past.length === 1) {
-          // undo to the initial state
-          retrievedState = initialState;
-          current = deepCopy(initialState);
-          return { past: [], future };
-        }
+        // else if (past.length === 1) {
+        //   console.log("undo to initial state");
+        //   // undo to the initial state
+        //   retrievedState = initialState;
+        //   current = deepCopy(initialState);
+        //   return { past: [], future };
+        // }
         const prevState = past[past.length - 1];
+        const nextState = current;
+        // debugger;
         retrievedState = prevState;
         current = prevState;
-        return { past: past.slice(0, -1), future: [current, ...future] }; // remove last element from past, add current to future
+        return {
+          past: past.slice(0, -1),
+          future: [nextState, ...future] 
+        }; // remove last element from past, add current to future
       });
+      // console.log("popping from history");
+      // console.log("action store length", Object.keys(get(flatActionStore)).length);
+      // console.log("undo queue length", get(historyStore).past.length);
+      // console.log("retrieved state", retrievedState);
+      // console.log("retrieved state note:", retrievedState.note);
       return retrievedState;
     },
     redo: (): Storable | null => {
@@ -71,9 +106,10 @@ function createHistoryStore() {
           return { past, future };
         }
         const nextState = future[0];
+        const prevState = current;
         retrievedState = nextState;
         current = nextState;
-        return { past: [...past, nextState], future: future.slice(1) };
+        return { past: [...past, prevState], future: future.slice(1) };
       });
       return retrievedState;
     },
@@ -84,65 +120,25 @@ function createHistoryStore() {
   };
 }
 
-// function createHistoryStore() {
-//   const { subscribe, update, set } = writable<{ past: Storable[], future: Storable[] }>({ past: [], future: [] });
+//is staged action not the only change?
+function differentOtherThanStagedAction(oldState: Storable, newState: Storable): boolean {
+  let oldWithoutStaged = Object.values(oldState.actionStore).filter(action => action.uuid !== oldState.stagedActionID);
+  let newWithoutStaged = Object.values(newState.actionStore).filter(action => action.uuid !== newState.stagedActionID);
 
-//   return {
-//     subscribe,
-//     push: (newState: Storable) => {
-//       update(({ past, future }) => {
-//         if (!deepEqual(current, newState)) {
-//           current = deepCopy(newState);
-//           return { past: [...past, current], future: [] };
-//         }
-//         return { past, future };
-//       });
-//     },
-//     pop: (): Storable | null => {
-//       let retrievedState: Storable | null = null;
-//       update(({ past, future }) => {
-//         if (past.length === 0) {
-//           return { past, future };
-//         } else if (past.length === 1) {
-//           retrievedState = initialState;
-//           current = deepCopy(initialState);
-//           return { past: [], future };
-//         }
-//         const prevState = past[past.length - 1];
-//         retrievedState = prevState;
-//         current = prevState;
-//         return { past: past.slice(0, -1), future: [current, ...future] };
-//       });
-//       return retrievedState;
-//     },
-//     redo: (): Storable | null => {
-//       let retrievedState: Storable | null = null;
-//       update(({ past, future }) => {
-//         if (future.length === 0) {
-//           return { past, future };
-//         }
-//         const nextState = future[0];
-//         retrievedState = nextState;
-//         current = nextState;
-//         return { past: [...past, nextState], future: future.slice(1) };
-//       });
-//       return retrievedState;
-//     },
-//     reset: () => {
-//       set({ past: [], future: [] });
-//     }
-//   };
-// }
-
-
+  return !deepEqual(oldWithoutStaged, newWithoutStaged);
+}
 
 export function saveToHistory(note: string) {
   // console.log("saving to history", note);
-  historyStore.push(copyCurrentState());
+  historyStore.push(copyCurrentState(note));
+  // console.log("action store length", Object.keys(get(flatActionStore)).length);
+  // console.log("undo queue:", get(historyStore).past);
+  // console.log("undo queue length", get(historyStore).past.length);
 }
 
-function copyCurrentState(): Storable {
+function copyCurrentState(note:string): Storable {
   return deepCopy({
+    note: note,
     actionStore: get(flatActionStore),
     stagedActionID: get(stagedActionID)
   });
