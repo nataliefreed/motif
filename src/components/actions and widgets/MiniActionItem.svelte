68af8c960b0e5b3bfd7734a4942c4c3e042a1a9f
Natalie Freed <saplingsonwheels@gmail.com>
2024-08-23 22:00:14 -0500

    mini action item styling experiments

--- src/components/actions and widgets/MiniActionItem.svelte
+++ src/components/actions and widgets/MiniActionItem.svelte
@@ -2,6 +2,7 @@
   import type { Action } from '../../types/types';
   import { onMount, createEventDispatcher } from 'svelte';
   import { updateActionParams, selectAction } from '../action-utils';
+  import PathWidget from './PathWidget.svelte';
 
   export let action: Action | null;
   const dispatch = createEventDispatcher();
@@ -45,16 +46,30 @@
     dispatch('miniActionClick', action.uuid);
   }
 
+  function constrain(value, min, max) {
+    return Math.min(Math.max(value, min), max);
+}
+
+  // Convert list of points to a string of SVG path commands
+  // M for move to start position, L for line to each point
+  function getPathData(path: [number, number][]) {
+    // console.log("getting path data", path);
+    if(!path || path.length === 0) path = [[0, 0]];
+    return path.map((pt, index) => 
+      `${index === 0 ? 'M' : 'L'} ${pt[0]},${pt[1]}`
+    ).join(' ');
+  }
+
 </script>
 
 {#if action}
+{@const size = 14}
   <!-- svelte-ignore a11y-click-events-have-key-events -->
   <span on:click={handleClick}>
-    {#if action.category === 'shapes'}
-      {@const size = 14}
+    {#if action.category === 'shapes' || action.name === 'straight line'}
       {#if action.name === 'circle'}
         <svg width="{size}" height="{size}" xmlns="http://www.w3.org/2000/svg">
-          <circle cx="{size / 2}" cy="{size / 2}" r="{size / 2 }" fill="{action.params.color}" />
+          <circle cx="{size / 2}" cy="{size / 2}" r="{constrain(action.params.radius, 0, size/2) }" fill="{action.params.color}" />
         </svg>
       {:else if action.name === 'rectangle'}
         {@const aspectRatio = action.params.width / action.params.height}
@@ -89,6 +104,16 @@
           <line x1="{0}" y1="{size/2}" x2="{size}" y2="{size/2}" stroke="{action.params.color}" stroke-width="3" />
         </svg>
       {/if}
+    {:else if action.name == 'along path'}
+      <svg width="{size}" height="{size}" viewBox="0 0 540 540" style="margin-right:{size/4}">
+        <path d={getPathData(action.params.path)} stroke="black" fill="none" />
+        {#each action.params.path as point, index}
+          {#if index > 0}
+            <line x1={action.params.path[index - 1][0]} y1={action.params.path[index - 1][1]} x2={point[0]} y2={point[1]} stroke="black" />
+          {/if}
+        <circle cx={point[0]} cy={point[1]} r="20" fill="black" />
+      {/each}
+      </svg>
     {/if}
 
 
@@ -99,7 +124,7 @@
 
 svg {
   transform: translateY(2px);
-  margin: 0 -2px;
+  /* margin: 0 -2px; */
   cursor: pointer;
 }
 
